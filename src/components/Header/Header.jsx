659a47b90c52bd3d5650c6d271cc75fafa52e392
Iuliia Kositcina <iuliiakors@gmail.com>
2024-04-25 22:47:08 -0400

    Add styling for header component (no functionality)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,57 +1,55 @@
 import "./Header.scss";
-import logo from "../../assets/logos/kijiji-logo.png";
+import logo from "../../assets/logos/kijiji-logo.svg";
 import search from "../../assets/icons/search.svg";
-import { useNavigate } from "react-router-dom";
+import categoryDots from "../../assets/icons/category-dots.svg";
+import chevronDown from "../../assets/icons/chevron-down.svg";
+import locaitonIcon from "../../assets/icons/location.svg";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Header() {
   const navigate = useNavigate();
 
-  const onUploadClick = () => {
-    navigate("/");
-  };
   return (
-    <header className="header-container">
-      <div className="header">
-        <a className="header__logo" href="/">
-          <img src={logo} alt="logo" />
-        </a>
-        <div className="header__search">
-          <div className="header__input-wrapper">
-            <img className="header__search-icon" src={search} alt="search" />
-            <input
-              className="header__search-input"
-              placeholder="What are you looking for?"
-            />
-            <select
-              name="category"
-              onChange={handleInputChange}
-              className="header__category-select"
-            >
-              <option value="">All categories</option>
-              <option value="Buy & Sell">Buy & Sell</option>
-              <option value="Cars & Vehicles">Cars & Vehicles</option>
-              <option value="Real Estate">Real Estate</option>
-              <option value="Jobs">Jobs</option>
-              <option value="Services">Services</option>
-              <option value="Pets">Pets</option>
-              <option value="Community">Community</option>
-              <option value="Vacation Rentals">Vacation Rentals</option>
-            </select>
-            <button>Search</button>
-          </div>
-          <div className="header__location">
-            <img />
-            <span>City of Toronto, Ontario</span>
-          </div>
-          <div className="header__wrapper">
-            <span>FR</span>
-            <span>Register</span>
-            <span>or</span>
-            <span>Sign In</span>
-          </div>
+    <header className="header">
+      <Link className="header__logo" href="/">
+        <img src={logo} alt="logo" />
+      </Link>
+      <div className="header__search">
+        <div className="header__input-wrapper">
+          <img className="header__icon" src={search} alt="search" />
+          <input
+            className="header__search-input"
+            placeholder="What are you looking for?"
+          />
+          <button className="header__category-button">
+            <img className="header__icon" src={categoryDots} />
+            All categories
+            <img className="header__icon" src={chevronDown} />
+          </button>
         </div>
-        <button>Post ad</button>
+
+        <button
+          className="header__search-button"
+          type="button"
+          onClick={() => {}}
+        >
+          Search
+        </button>
       </div>
+
+      <button className="header__location">
+        <img className="header__icon" src={locaitonIcon} />
+        <span>City of Toronto, Ontario</span>
+      </button>
+
+      <button className="header__language">FR</button>
+      <div className="header__auth">
+        <button>Register</button>
+        <span>or</span>
+        <button>Sign In</button>
+      </div>
+
+      <button className="header__post-ad-button">Post ad</button>
     </header>
   );
 }

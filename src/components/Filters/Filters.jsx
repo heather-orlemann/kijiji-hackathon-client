bb435499e3932cbd0028b5cd163076c8797c793a
Rui Zou <zourui0107@gmail.com>
2024-04-25 20:36:55 -0400

    set up filter component

---
+++ src/components/Filters/Filters.jsx
@@ -0,0 +1,19 @@
+
+const Filters = () => {
+    return (
+        <div className="filters">
+            <div className="filters__title filters__header ">
+                <p>Filters</p>
+            </div>
+
+            <div className="filters__title filters__container">
+                </div>
+
+            <div className="filters__title filters__location">
+                <p>Location</p>
+
+        </div>
+    );
+};
+
+export default Filters;
---
+++ src/pages/MingaListPage/MingaListPage.jsx
@@ -0,0 +1,20 @@
+import './MingaListings.scss';  
+
+const MingaListPage = () => {
+    return (
+        <div className='MingaListPage'>
+            <div className='MingaListPage__title'>
+                <h1>Minga in Toronto</h1>
+            </div>
+            <div className='MingaListPage__container'>
+            <div className='MingaListPage__filters'>
+                
+                </div>
+            <div className='MingaListPage__list'>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MingaListPage;

3ca52f2598bd67a56f01ec4a17f9671c1010d75f
Maria Kovacevic <maria.kovac22@gmail.com>
2024-04-25 22:23:02 -0400

    minga details component and page creation

---
+++ src/pages/MingaDetails/MingaDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../MingaDetails/MingaDetails.scss'
+import MingaDetailsComponent from '../../components/MingaDetailsComponent'
+
+const MingaDetails=()=>{
+    return(
+        <div>
+         < MingaDetailsComponent />
+        </div>
+    )
+}
